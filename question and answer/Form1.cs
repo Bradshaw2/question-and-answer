5858cb5963cd17101843fb13a1c95552397ea174
Shaw <rc0027629@student.riversidecollege.ac.uk>
2017-09-26 13:43:21 +0100

    Add project files.

---
+++ question and answer/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace question_and_answer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("welcome " + textBox1.Text + "question: 17 - 2 x 8 = 1, Yes or No?"
+                );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("correct answer + textBox1.");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("incorrect answer + textBox1.");
+        }
+    }
+}
